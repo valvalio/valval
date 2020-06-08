7b2a69bed54fbde46f64d69fe5dc51ca315cbaad
taojy123 <taojy123@163.com>
2020-06-08 11:07:50 +0800

    Update valval.v

--- valval.v
+++ valval.v
@@ -11,7 +11,7 @@ import strings
 
 const (
 	VERSION = '0.1.4'
-	V_VERSION = '0.1.25'
+	V_VERSION = '0.1.24'
 	HTTP_404 = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404 Not Found'
 	HTTP_413 = 'HTTP/1.1 413 Request Entity Too Large\r\nContent-Type: text/plain\r\n\r\n413 Request Entity Too Large'
 	HTTP_500 = 'HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/plain\r\n\r\n500 Internal Server Error'
